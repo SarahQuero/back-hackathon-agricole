1deb576809bc0eea1b4c4666bb0dd21b39095909
FlorentLem <florentlempro@gmail.com>
2021-01-13 22:29:32 +0100

    zipcode added

--- models/agri.js
+++ models/agri.js
@@ -2,7 +2,7 @@
 const db = require('../db');
 
 const getAllAgri = async () => {
-    const res = await db.query("SELECT registered_at, agriculteurs.id, farmsize, city, latitude, longitude FROM agriculteurs, villes WHERE agriculteurs.villes_id = villes.id", [])
+    const res = await db.query("SELECT registered_at, agriculteurs.id, farmsize, zipcode, city, latitude, longitude FROM agriculteurs, villes WHERE agriculteurs.villes_id = villes.id", [])
     const trans = await db.query("SELECT agriculteurs_id AS idagri, category FROM transactions, produits WHERE transactions.produits_id = produits.id");
     trans.map((item) => {
         if (!res[item.idagri].type) {
@@ -17,7 +17,7 @@ const getAllAgri = async () => {
 }
 
 const getAgriById = async (id) => {
-    const res = await db.query("SELECT registered_at, agriculteurs.id, farmsize, city, latitude, longitude FROM agriculteurs, villes WHERE agriculteurs.villes_id = villes.id AND agriculteurs.id = ?", [id]);
+    const res = await db.query("SELECT registered_at, agriculteurs.id, farmsize, zipcode, city, latitude, longitude FROM agriculteurs, villes WHERE agriculteurs.villes_id = villes.id AND agriculteurs.id = ?", [id]);
     res[0].transac = await db.query("SELECT created_at, price, quantity, name, category FROM transactions, produits WHERE transactions.produits_id = produits.id AND agriculteurs_id = ?", [id]);
     return res[0];
 }

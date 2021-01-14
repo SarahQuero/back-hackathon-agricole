9d107da4176888ed7c0216cf9a70e7c596d67849
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2021-01-14 13:53:16 +0100

    Add acheteurs all

--- models/acheteurs.js
+++ models/acheteurs.js
@@ -5,10 +5,10 @@ const getProfilAcheteur = async id => {
     'SELECT a.name, a.type, count(t.price) AS nb, ROUND(SUM(t.quantity),2) AS somme FROM acheteurs AS a JOIN transactions AS t ON a.id=t.acheteurs_id WHERE a.id=?',
     [id]
   );
-  res.map((el) => {
-    el.role=false;
-    return el; 
-  })
+  res.map(el => {
+    el.role = false;
+    return el;
+  });
   return res;
 };
 
@@ -20,7 +20,35 @@ const getTransAcheteur = async id => {
   return trans;
 };
 
+const getAllAcheteurs = async () => {
+  const res = await db.query(
+    'SELECT acheteurs.id, name, zipcode, city, latitude, longitude FROM acheteurs, villes WHERE acheteurs.villes_id = villes.id',
+    []
+  );
+  const trans = await db.query(
+    'SELECT acheteurs_id AS idacheteur, category FROM transactions, produits WHERE transactions.produits_id = produits.id'
+  );
+  trans.map(item => {
+    if (!res[item.idacheteur]) {
+      return item;
+    }
+    if (!res[item.idacheteur].type) {
+      res[item.idacheteur].type = [];
+    }
+    if (!res[item.idacheteur].type.includes(item.category)) {
+      res[item.idacheteur].type.push(item.category);
+    }
+    return item;
+  });
+  res.map(el => {
+    el.role = true;
+    return el;
+  });
+  return res;
+};
+
 module.exports = {
   getProfilAcheteur,
-  getTransAcheteur
+  getTransAcheteur,
+  getAllAcheteurs
 };

8386de9ae5f1f3e91015d01b57090f28609f8285
Damien Mauger <mauger.damien@hotmail.fr>
2021-01-14 11:40:34 +0100

    ajout de la route pour récupérer le profil agriculteur

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const agri = require('./agri.routes.js');
+const profil = require('./profil.routes.js');
 
 router.use('/agri', agri);
+router.use('/profil', profil);
 
 router.get('/', (req, res) => {
   res.status(200).json({msg: "get /api"})

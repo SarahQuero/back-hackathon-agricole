18830768b1d62d5123ca7bc10b7524da53d0eb09
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2021-01-14 12:04:41 +0100

    Add route acheteurs

--- routes/acheteurs.routes.js
+++ routes/acheteurs.routes.js
@@ -1,20 +1,10 @@
 const router = require('express').Router();
-const db = require('../db');
+const { getProfilAcheteur, getTransAcheteur } = require('../models/acheteurs');
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  const sql =
-    'SELECT a.name, a.type, count(t.price) AS nb, ROUND(SUM(t.quantity),2) AS somme FROM acheteurs AS a JOIN transactions AS t ON a.id=t.acheteurs_id WHERE a.id=?';
-  db.query(sql, [id], (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ errorMessage: 'internal error server' });
-    } else {
-      res.status(200).send(result);
-    }
-  });
+router.get('/:id', async (req, res) => {
+  const profil = await getProfilAcheteur(req.params.id);
+  const transAcheteurs = await getTransAcheteur(req.params.id);
+  res.status(200).json({ profil: profil, transAcheteurs: transAcheteurs });
 });
-
-
 
 module.exports = router;
